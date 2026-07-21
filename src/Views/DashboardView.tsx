adc73c8aa5ab7094ab4501fe9e1b7ef91f5106e4
Victor Varela <victor398@gmail.com>
2026-07-21 11:25:15 -0300

    feat: integrate react-hook-form for project creation and add error handling

--- src/Views/DashboardView.tsx
+++ src/Views/DashboardView.tsx
@@ -14,7 +14,7 @@ export default function DashboardView() {
 /**
  * Esta vista es HIJA de AppLayout por eso AppLayout lleva OUTLET
  * 
- * Usamos Link de para llevar a otra pagina- De nuevo, el router es quien manda-- cada link en una view debe estar en el router-- asi es la dinamica: hacemos una vista-> presentamos datos-> capturamos datos-> llevamos a otra pagina(ROUTER), etc etc Aca llevo a user a CREAR PROYECTO, seguramente cuando hayan proyectos van a aparecer aqui pero siguiendo el CRUD primero es CREAR-
+ * Usamos Link de react-router-dom para llevar a otra pagina- De nuevo, el router es quien manda-- cada link en una view debe estar en el router-- asi es la dinamica: hacemos una vista-> presentamos datos-> capturamos datos-> llevamos a otra pagina(ROUTER), etc etc Aca llevo a user a CREAR PROYECTO, seguramente cuando hayan proyectos van a aparecer aqui pero siguiendo el CRUD primero es CREAR-
  * La cuestion es saber diferenciar CUANDO crear un vista--> aca llevamos a user a una pagina asi que tal vez se puede decir que si llevamos a un lugar eso lo tenemos que crear como vista y lo hacemos en las carpetas correspondientes Views/CreateProjectView.tsx
 
 
---
+++ src/components/projects/ProjectForm.tsx
@@ -0,0 +1,73 @@
+import ErrorMessage from "../ErrorMessage";
+import type { FieldErrors, UseFormRegister } from "react-hook-form";
+
+type ProjectFormProps = {
+  errors: FieldErrors<{
+    projectName: string;
+    clientName: string;
+    description: string;
+  }>;
+  register: UseFormRegister<{
+    projectName: string;
+    clientName: string;
+    description: string;
+  }>;
+};
+
+//Usamos intellisense de VsCode, hovereamos en register y errors en CreateProjectView y tenemos su TYPE- tenemos que importar igualmente esos types de react-hook-form para que los reconozca TS.
+
+export default function ProjectForm({ errors, register }: ProjectFormProps) {
+  return (
+    <>
+      <div className="mb-5 space-y-3">
+        <label htmlFor="projectName" className="text-sm uppercase font-bold">
+          Nombre del Proyecto
+        </label>
+        <input
+          id="projectName"
+          className="w-full p-3  border border-gray-200"
+          type="text"
+          placeholder="Nombre del Proyecto"
+          {...register("projectName", {
+            required: "El Titulo del Proyecto es obligatorio",
+          })}
+        />
+
+        {errors.projectName && <ErrorMessage>{errors.projectName.message}</ErrorMessage>}
+      </div>
+
+      <div className="mb-5 space-y-3">
+        <label htmlFor="clientName" className="text-sm uppercase font-bold">
+          Nombre Cliente
+        </label>
+        <input
+          id="clientName"
+          className="w-full p-3  border border-gray-200"
+          type="text"
+          placeholder="Nombre del Cliente"
+          {...register("clientName", {
+            required: "El Nombre del Cliente es obligatorio",
+          })}
+        />
+
+        {errors.clientName && <ErrorMessage>{errors.clientName.message}</ErrorMessage>}
+      </div>
+
+      <div className="mb-5 space-y-3">
+        <label htmlFor="description" className="text-sm uppercase font-bold">
+          Descripción
+        </label>
+        <textarea
+          id="description"
+          className="w-full p-3  border border-gray-200"
+          placeholder="Descripción del Proyecto"
+          {...register("description", {
+            required: "Una descripción del proyecto es obligatoria",
+          })}
+        />
+
+        {errors.description && <ErrorMessage>{errors.description.message}</ErrorMessage>}
+      </div>
+    </>
+  );
+}
